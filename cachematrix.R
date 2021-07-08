704fce3f131392451c374e90e0b0ea42046e50b8
Eunice Penetrante <ekppenetrante@mymail.mapua.edu.ph>
2021-07-08 13:18:48 +0800

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## for my matrix i set the input yAnG
+## for the solved value "aNNa" as null
+##
+## consequently i changed "matrix" to "solve"
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(yAnG = matrix(sample(2:100,8),6,6)) {
+aNNa <- NULL
+set <- function(y) {
+yAnG <- y
+aNNa <- NULL
 }
+get <- function() yAnG
+setsolve <- function(solve) aNNa <<- solve
+getsolve <- function() aNNa
+list(set = get, get = get,
+setsolve = setsolve,
+getsolve = getsolve)}
 
-
-## Write a short comment describing this function
+## for the computation of chache and inverse of matrix
+## see below
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'yAnG'
+yAng <- x$getsolve()
+if(!is.null(aNNa)) {
+message("get the inverse of the matrix")
+return(aNNa) }
+data <- x$get()
+aNNa <- solve(data, ...)
+x$setsolve(aNNa)
+aNNa 
 }
